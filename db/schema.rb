ee4719c0077be96ea18e6bf3d1ad181bc4f18caf
1krisortiz1 <itskristin.ortiz@gmail.com>
2019-12-14 13:20:19 -0700

    Integrated technologies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_155736) do
+ActiveRecord::Schema.define(version: 2019_12_14_164447) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 2019_12_14_155736) do
     t.text "badge"
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.integer "portfolio_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
@@ -61,4 +69,5 @@ ActiveRecord::Schema.define(version: 2019_12_14_155736) do
   end
 
   add_foreign_key "blogs", "topics"
+  add_foreign_key "technologies", "portfolios"
 end
