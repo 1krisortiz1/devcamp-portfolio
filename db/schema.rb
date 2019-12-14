f810116beda065deec8c856cfa108371350f660b
1krisortiz1 <itskristin.ortiz@gmail.com>
2019-12-14 09:09:53 -0700

    Implemented concern for maanaging image data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_022319) do
+ActiveRecord::Schema.define(version: 2019_12_14_155736) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_12_14_022319) do
     t.integer "percent_utilized"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
